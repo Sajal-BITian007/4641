a23e60c76a7dc78f24fb775df6d032423fcd6f0e
Sajal-BITian007 <72162523+Sajal-BITian007@users.noreply.github.com>
2020-09-30 12:51:55 -0700

    Update featureExtractors.py

--- assignment2/featureExtractors.py
+++ assignment2/featureExtractors.py
@@ -5,7 +5,7 @@
 # purposes. The Pacman AI projects were developed at UC Berkeley, primarily by
 # John DeNero (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
 # For more info, see http://inst.eecs.berkeley.edu/~cs188/sp09/pacman.html
-
+# Don't change anything unless you are 100% sure....
 "Feature extractors for Pacman game states"
 
 from game import Directions, Actions
